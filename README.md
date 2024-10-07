9eda3738c8ca303cdcca8ae510e8f7ec3cc15775
Shashwat1524 <shashwat.a@learner.manipal.edu>
2024-10-07 14:18:38 -0400

    update

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Lane_Detection_OpenCV_Tensor_Flow
 
-### This is a repository which is used for Lane Dettection using Open CV and Deep Learning
+### This is a repository which is used for Lane Dettection using Open CV and Deep Learning under development
--- lanes.py
+++ lanes.py
@@ -22,13 +22,52 @@ def region_of_interest(image):
     masked_image=cv2.bitwise_and(image,mask)
     return masked_image
 
+def display_lines(image, lines):
+    line_image = np.zeros_like(image)
+    if lines is None or len(lines) == 0:  # Check if lines are detected
+        return line_image  # Return an empty image if no lines are found
+    for x1,y1,x2,y2 in lines:
+        cv2.line(line_image, (int(x1), int(y1)), (int(x2), int(y2)), (255, 0, 0), 10)
+    return line_image
+
+def average_slope_intercept(image,lines):
+    left_fit=[]
+    right_fit=[]
+    for line in lines:
+        x1,y1,x2,y2=line.reshape(4)
+        parameters=np.polyfit((x1,x2),(y1,y2),1) #y=mx+b and return vector of slope and coeff that describe y intercept
+        slope=parameters[0]
+        intercept=parameters[1]
+        if slope < 0:
+            left_fit.append((slope,intercept))
+        else:
+            right_fit.append((slope,intercept))
+    
+    left_fit_average=np.average(left_fit,axis=0)
+    right_fit_average=np.average(right_fit,axis=0)
+    left_line=make_coordinates(image,left_fit_average)
+    right_line=make_coordinates(image,right_fit_average)
+    return np.array([left_line,right_line])
+
+def make_coordinates(image,line_parameters):
+    slope,intercept=line_parameters
+    y1=image.shape[0]
+    y2=int(y1*(3/5))
+    x1=(y1-intercept)/slope
+    x2=(y2-intercept)/slope
+    return np.array([x1,y1,x2,y2])
+
 
 image=cv2.imread('test_image.jpg')
 # cv2.imshow('Lane Image',image)
 lane_image=np.copy(image)
-canny=canny(lane_image)
-cropped_image=region_of_interest(canny)
-cv2.imshow("Result",cropped_image)
+canny_image=canny(lane_image)
+cropped_image=region_of_interest(canny_image)
+lines=cv2.HoughLinesP(cropped_image,2,np.pi/180,100,np.array([]),minLineLength=40,maxLineGap=5)
+averaged_lines=average_slope_intercept(lane_image,lines)
+line_image=display_lines(lane_image,averaged_lines)
+combo_image=cv2.addWeighted(lane_image, 0.8, line_image,1,1)
+cv2.imshow("Result",combo_image)
 cv2.waitKey(10000)
 
 #Edge is detected by rapid change in Brightness
